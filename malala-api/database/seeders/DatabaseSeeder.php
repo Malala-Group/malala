e6c7a74acd536083516b61b55afddf2835d6deb9
Juan Prasetyo <juanprasetyo000@gmail.com>
2023-11-17 11:19:19 +0700

    initial commit

---
+++ malala-api/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Database\Seeders\RegencySeeder;
+use Database\Seeders\VillageSeeder;
+use Database\Seeders\DistrictSeeder;
+use Database\Seeders\ProvinceSeeder;
+use Database\Seeders\RegionTypeSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        
+    }
+}
